a541d74707f68d488e3a1138fcc00a0fdb2e0c41
Juanjo Bazán <jjbazan@gmail.com>
2024-10-17 14:05:57 +0200

    Update tap.py

--- scripts/tap.py
+++ scripts/tap.py
@@ -1,14 +1,9 @@
 from astroquery.utils.tap.core import Tap
 
-
 def query_tap_table(tap_url, query):
     tap_service = Tap(url=tap_url)
     result = tap_service.launch_job(query)
     return result.get_results()
-
-
-
-
 
 def query_esa_cluster_table(query):
     esa_cluster_tap_url = "https://csa.esac.esa.int/csa-sl-tap/data"
